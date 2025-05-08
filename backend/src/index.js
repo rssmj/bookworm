b2dbb2d2d38e1b6618ec53364a305356d5c14ad3
rssmj <58471626+rssmj@users.noreply.github.com>
2025-05-07 23:17:16 -0400

    add dotenv

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,7 +1,11 @@
 import express from 'express';
+import 'dotenv/config';
 
-const app = express();
+const app = express(); // Initialize express app
+const PORT = process.env.PORT || 3000; // Default port is 3000 if not specified in .env
 
-app.listen(3000, () => {
-  console.log('Server is running on port 3000');
-});
+console.log({ PORT }); // Log the port to verify it's being set correctly
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+}); // Start the server and listen on the specified port
