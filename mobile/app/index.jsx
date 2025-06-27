acc715a920c259692b4b838df2cb7f8004deaa42
rssmj <58471626+rssmj@users.noreply.github.com>
2025-06-27 00:37:33 -0400

    add mobile layouts, styles, constants

---
+++ mobile/app/index.jsx
@@ -0,0 +1,23 @@
+import { StyleSheet, Text, View } from 'react-native';
+import { Link } from 'expo-router';
+
+export default function Index() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>welcome sup loser ha you suck na na</Text>
+      <Link href='/signup'>Signup</Link>
+      <Link href='/login'>Login</Link>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  title: {
+    color: 'blue',
+  },
+});
